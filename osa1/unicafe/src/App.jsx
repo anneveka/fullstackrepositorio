a17992ab533b33be38a7543946d8f81c1841632c
anneveka <anneveka@jyu.fi>
2025-09-16 13:58:14 +0300

    tehtävä 1.8

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,64 +5,38 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [goodtotal, setTotalGood] = useState(0)
-  const [neutraltotal, setTotalNeutral] = useState(0)
-  const [badtotal, setTotalBad] = useState(0)
-  const [total, setTotal] = useState(0)
 
-  const handleGood = () => {
-    setGood(good + 1)
-    const updeateGood = good + 1
-    setTotalGood(updeateGood)
-    setTotal(updeateGood + neutral + bad)
-  }
-
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    const updateNeutral = neutral + 1
-    setTotalNeutral(updateNeutral)
-    setTotal(updateNeutral + good + bad)
-  }
-
-  const handleBad = () => {
-    setBad(bad + 1)
-    const updateBad = bad + 1
-    setTotalBad(updateBad)
-    setTotal(updateBad + good + neutral)
-  }
-
+  const total = good + neutral + bad
   const average = total === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / total
+  const positive = total === 0 ? 0 : (good/total)*100
+
+  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
+      <Button onClick={() => setGood(good+1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral+1)} text="neutral"/>
+      <Button onClick={() => setBad(bad+1)} text="bad"/>
       <h1>statistics</h1>
-      <p>good: {goodtotal}</p>
-      <p>neutral: {neutraltotal}</p>
-      <p>bad: {badtotal}</p>
-      <p>all: {total}</p>
-      <p>average: {average}</p>
-      <Positive good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} average={average} positive={positive}/>
     </div>
   )
 }
 
-
-const Positive = (props) => {
-  const good = props.good
-  const neutral = props.neutral
-  const bad = props.bad
-
-  const yhteensa = good + neutral + bad
-
+const Statistics = (props) => {
+  
   return (
     <div>
-      <p>positive: {(good/yhteensa)*100} %</p>
+      <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>all: {props.good + props.neutral +props.bad}</p>
+      <p>average: {props.average}</p>
+      <p>positive: {props.positive}%</p>
     </div>
   )
+
 }
 
 export default App
