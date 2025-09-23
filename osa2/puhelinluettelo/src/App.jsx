af66e78f4199b34230c5b6f93bf5e6f5db0a7cc6
anneveka <anneveka@jyu.fi>
2025-09-23 12:00:15 +0300

    2.8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,41 +2,44 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: "040-1231244"}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addNameAndNumber = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName,
+      name: newName, //määritellään uusi nimi
+      number: newNumber // ja numero
     }
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    if (persons.some(person => person.name === newName || person.number === newNumber)) { //tarkistetaan onko numeroa tai nimeä jo kirjoilla
+      alert(`${newName} is already added to phonebook`) 
     } else{
       setPersons(persons.concat(nameObject))
-      setNewName('')
+      setNewName('') 
+      setNewNumber('')
     }
   }
 
-  const handleName = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleName = (event) => {setNewName(event.target.value)}
+
+  const handleNumber = (event) => {setNewNumber(event.target.value)}
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <input value={newName} onChange={handleName} />
+      <form onSubmit={addNameAndNumber}>
+        <div>name: <input value={newName} onChange={handleName}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumber}/></div>
         <button type="submit">add</button>
       </form>
-      <ul>
-        {persons.map((person) =>
-          <li key={person.name}>{person.name}</li> 
-        )}
-      </ul>
       <h2>Numbers</h2>
-      ...
+      <p>
+        {persons.map((person) =>
+          <li key={person.name}>{person.name} {person.number}</li> 
+        )}
+      </p>
     </div>
   )
 
