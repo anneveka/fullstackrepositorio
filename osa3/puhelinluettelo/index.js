f9142e169a5944ec44ff0b3141c710410f7f607d
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-04-10 10:14:23 +0300

    tehtävä 3.3

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -3,22 +3,22 @@ const app = express();
 
 let persons = [
     {
-        id: 1,
+        id: "1",
         name: "Arto Hellas",
         number: "040-123456",
     },
     {
-        id: 2,
+        id: "2",
         name: "Ada Lovelace",
         number: "39-44-5323523",
     },
     {
-        id: 3,
+        id: "3",
         name: "Dan Abramov",
         number: "123-456-789",
     },
     {
-        id: 4,
+        id: "4",
         name: "Mary Poppendieck",
         number: "39-23-6423122",
     },
@@ -41,6 +41,7 @@ app.get("/api/persons/:id", (request, response) => {
     if (person) {
         response.json(person);
     } else {
+        console.log("Person not found with id:", id);
         response.status(404).end();
     }
 });
@@ -50,45 +51,6 @@ app.get("/info", (request, response) => {
     response.end(
         "Phonebook has info for " + persons.length + " people\n" + new Date(),
     );
-});
-
-const generateId = () => {
-    const maxId =
-        persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0;
-    return String(maxId + 1);
-};
-
-app.post("/api/persons", (request, response) => {
-    const body = request.body;
-
-    if (!body.name) {
-        return response.status(400).json({
-            error: "name missing",
-        });
-    }
-
-    if (!body.number) {
-        return response.status(400).json({
-            error: "number missing",
-        });
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId(),
-    };
-
-    persons = persons.concat(person);
-
-    response.json(person);
-});
-
-app.delete("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    notes = notes.filter((note) => note.id !== id);
-
-    response.status(204).end();
 });
 
 const PORT = 3001;
