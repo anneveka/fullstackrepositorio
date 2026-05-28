364b4bda2dcb285c8590b88f4f1d415275f9700f
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-05-28 11:48:12 +0300

    tehtävä 3.13

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,70 +1,37 @@
+require("dotenv").config();
 const express = require("express");
+const Person = require("./models/person");
+
 const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "123-456-789",
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
+let persons = [];
 
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method);
+    console.log("Path:  ", request.path);
+    console.log("Body:  ", request.body);
+    console.log("---");
+    next();
+};
+
+app.use(requestLogger);
 app.use(express.json());
-app.use(cors());
 app.use(express.static("dist"));
-
-morgan.token("body", function (req, res) {
-    return req.body ? JSON.stringify(req.body) : "";
-});
-
-app.use(
-    morgan(
-        ":method :url :status :res[content-length] - :response-time ms: :body",
-    ),
-);
 
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons);
+    });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    const person = persons.find((person) => person.id === id);
-
-    if (person) {
+    Person.findById(request.params.id).then((person) => {
         response.json(person);
-    } else {
-        console.log("Person not found with id:", id);
-        response.status(404).end();
-    }
-});
-
-app.get("/info", (request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end(
-        "Phonebook has info for " + persons.length + " people\n" + new Date(),
-    );
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -100,18 +67,18 @@ app.post("/api/persons", (request, response) => {
         });
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
         id: generateId(),
-    };
+    });
 
-    persons = persons.concat(person);
-
-    response.json(person);
+    person.save().then((savedPerson) => {
+        response.json(savedPerson);
+    });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
