eb0a65fa9cf3c0b8c51b88911b82ef2152343977
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-05-28 12:48:39 +0300

    muutoksia 3 osaa varten

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,165 +1,191 @@
-import { useState, useEffect } from 'react'
-import personService from './services/persons'
-import { createWebSocketModuleRunnerTransport } from 'vite/module-runner'
-import axios from 'axios'
-import './index.css'
+import { useState, useEffect } from "react";
+import personService from "./services/persons";
+import { createWebSocketModuleRunnerTransport } from "vite/module-runner";
+import axios from "axios";
+import "./index.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [search, setSearch] = useState('')
-  const [infoMessage, setInfoMessage] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
+    const [search, setSearch] = useState("");
+    const [infoMessage, setInfoMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-  useEffect(() => {
-    personService
-      .getAll()
-      .then(response => {
-        setPersons(response.data)
-      })
-  }, [])
+    useEffect(() => {
+        personService.getAll().then((initialPersons) => {
+            setPersons(initialPersons);
+        });
+    }, []);
 
-  const addNameAndNumber = (event) => {
-    event.preventDefault()
-    const nameObject = {
-      name: newName, //määritellään uusi nimi
-      number: newNumber // ja numero
-    }
+    const addNameAndNumber = (event) => {
+        event.preventDefault();
+        const nameObject = {
+            name: newName, //määritellään uusi nimi
+            number: newNumber, // ja numero
+        };
 
-    if (persons.some(person => person.name === newName && person.number === newNumber)) { //tarkistetaan onko numeroa tai nimeä jo kirjoilla
-      alert(`${newName} is already added to phonebook`) 
-    } 
-    
-    const person = persons.find(person => person.name === newName)
-    if (persons.some(person => person.name === newName && person.number !== newNumber)) {
-      if(confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
-        personService
-          .update(person.id, nameObject)
-          .catch(error => {
-            setErrorMessage(`Information of ${person.name} has already been removed from server`)
+        if (
+            persons.some(
+                (person) =>
+                    person.name === newName && person.number === newNumber,
+            )
+        ) {
+            //tarkistetaan onko numeroa tai nimeä jo kirjoilla
+            alert(`${newName} is already added to phonebook`);
+        }
+
+        const person = persons.find((person) => person.name === newName);
+        if (
+            persons.some(
+                (person) =>
+                    person.name === newName && person.number !== newNumber,
+            )
+        ) {
+            if (
+                confirm(
+                    `${newName} is already added to phonebook, replace the old number with a new one?`,
+                )
+            ) {
+                personService
+                    .update(person.id, nameObject)
+                    .catch((error) => {
+                        setErrorMessage(
+                            `Information of ${person.name} has already been removed from server`,
+                        );
+                        setTimeout(() => {
+                            setErrorMessage(null);
+                        }, 5000);
+                    })
+                    .then((response) => {
+                        setPersons(
+                            persons.map((p) =>
+                                p.id !== person.id ? p : response.data,
+                            ),
+                        );
+                        setNewName("");
+                        setNewNumber("");
+                        setInfoMessage(`Changed ${newName}'s number`);
+                        setTimeout(() => {
+                            setInfoMessage(null);
+                        }, 5000);
+                    });
+            }
+        } else {
+            personService.create(nameObject).then((response) => {
+                setPersons(persons.concat(response));
+                setNewName("");
+                setNewNumber("");
+            });
+            setInfoMessage(`Added ${newName}`);
             setTimeout(() => {
-              setErrorMessage(null)
-            }, 5000)
-          })
-          .then(response => {
-          setPersons(persons.map(p => p.id !== person.id ? p : response.data))
-          setNewName('')
-          setNewNumber('')
-          setInfoMessage(`Changed ${newName}'s number`)
-          setTimeout(() => {
-            setInfoMessage(null)
-          }, 5000)
-        })
-      }
-    }
-    else{
-      personService
-        .create(nameObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
-          setNewName('')
-          setNewNumber('')
-        })
-        setInfoMessage(`Added ${newName}`)
-        setTimeout(() => {
-            setInfoMessage(null)
-        }, 5000)
-    }
-  }
+                setInfoMessage(null);
+            }, 5000);
+        }
+    };
 
-  const filteredPersons = persons.filter(person =>
-    person.name.toLowerCase().includes(search.toLowerCase()) //filteröidään hakuun sopivat nimet
-  )
+    const filteredPersons = persons.filter(
+        (person) => person.name.toLowerCase().includes(search.toLowerCase()), //filteröidään hakuun sopivat nimet
+    );
 
-  const handleSearch = (event) => {setSearch(event.target.value)}
-  const handleName = (event) => {setNewName(event.target.value)}
-  const handleNumber = (event) => {setNewNumber(event.target.value)}
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    };
+    const handleName = (event) => {
+        setNewName(event.target.value);
+    };
+    const handleNumber = (event) => {
+        setNewNumber(event.target.value);
+    };
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Notification message={infoMessage} />
-      <ErrorNotification message={errorMessage} />
-      <Filter search={search} handleSearch={handleSearch} />
-      <h2>add a new</h2>
-      <PersonForm 
-        newName={newName} handleName={handleName} 
-        newNumber={newNumber} handleNumber={handleNumber}
-        addNameAndNumber={addNameAndNumber}
-      />
-      <h3>Numbers</h3>
-      <p>
-        {filteredPersons.map(person =>
-           <Persons key={person.name} person={person} setPersons={setPersons} setInfoMessage={setInfoMessage}/>
-        )}
-      </p>
-    </div>
-  )
-
-}
-
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Notification message={infoMessage} />
+            <ErrorNotification message={errorMessage} />
+            <Filter search={search} handleSearch={handleSearch} />
+            <h2>add a new</h2>
+            <PersonForm
+                newName={newName}
+                handleName={handleName}
+                newNumber={newNumber}
+                handleNumber={handleNumber}
+                addNameAndNumber={addNameAndNumber}
+            />
+            <h3>Numbers</h3>
+            <p>
+                {filteredPersons.map((person) => (
+                    <Persons
+                        key={person.name}
+                        person={person}
+                        setPersons={setPersons}
+                        setInfoMessage={setInfoMessage}
+                    />
+                ))}
+            </p>
+        </div>
+    );
+};
 
 const Persons = (props) => {
+    const deletePerson = () => {
+        if (confirm(`Delete ${props.person.name}?`)) {
+            personService.remove(props.person.id).then((response) => {
+                props.setPersons((theRest) =>
+                    theRest.filter((person) => person.id !== props.person.id),
+                );
+            });
+            props.setInfoMessage(`Removed ${props.person.name}`);
+            setTimeout(() => {
+                props.setInfoMessage(null);
+            }, 5000);
+        }
+    };
 
-  const deletePerson = () => {
-    if(confirm(`Delete ${props.person.name}?`)) {
-    personService
-      .remove(props.person.id)
-      .then(response => {
-        props.setPersons(theRest => 
-          theRest.filter(person => person.id !== props.person.id)
-        )
-      })
-      props.setInfoMessage(`Removed ${props.person.name}`)
-      setTimeout(() => {
-          props.setInfoMessage(null)
-      }, 5000)
-    }
-  }
-
-  return (
-    <li>
-      {props.person.name} {props.person.number}  <button onClick={() => deletePerson()}>delete</button>
-    </li>
-  )
-}
+    return (
+        <li>
+            {props.person.name} {props.person.number}{" "}
+            <button onClick={() => deletePerson()}>delete</button>
+        </li>
+    );
+};
 
 const PersonForm = (props) => {
-  return(
-    <form onSubmit={props.addNameAndNumber}>
-      <div>name: <input value={props.newName} onChange={props.handleName}/></div>
-      <div>number: <input value={props.newNumber} onChange={props.handleNumber}/></div>
-      <button type="submit">add</button>
-    </form>
-  )
-}
+    return (
+        <form onSubmit={props.addNameAndNumber}>
+            <div>
+                name:{" "}
+                <input value={props.newName} onChange={props.handleName} />
+            </div>
+            <div>
+                number:{" "}
+                <input value={props.newNumber} onChange={props.handleNumber} />
+            </div>
+            <button type="submit">add</button>
+        </form>
+    );
+};
 
-const Filter = ({search, handleSearch}) => {
-  return(
-  <div>
-    filter shown with <input value={search} onChange={handleSearch} />
-  </div>
-  )
-}
+const Filter = ({ search, handleSearch }) => {
+    return (
+        <div>
+            filter shown with <input value={search} onChange={handleSearch} />
+        </div>
+    );
+};
 
-const Notification = ({message}) => {
-  if (message === null) {return null}
-  return (
-    <div className='ilmoitus'>
-      {message}
-    </div>
-  )
-}
+const Notification = ({ message }) => {
+    if (message === null) {
+        return null;
+    }
+    return <div className="ilmoitus">{message}</div>;
+};
 
-const ErrorNotification = ({message}) => {
-  if (message === null) {return null}
-  return (
-    <div className='error'>
-      {message}
-    </div>
-  )
-}
+const ErrorNotification = ({ message }) => {
+    if (message === null) {
+        return null;
+    }
+    return <div className="error">{message}</div>;
+};
 
-
-export default App
+export default App;
