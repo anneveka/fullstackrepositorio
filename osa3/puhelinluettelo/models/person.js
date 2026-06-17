b8a6a15c620ca95a832a3a82e055deed50966846
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-06-17 09:12:16 +0300

    tehtävä 3.20*

--- osa3/puhelinluettelo/models/person.js
+++ osa3/puhelinluettelo/models/person.js
@@ -19,7 +19,17 @@ const personSchema = new mongoose.Schema({
         type: String,
         minlength: 3,
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{3}-\d{5}/.test(v);
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
+        required: [true, "User phone number required"],
+    },
 });
 
 personSchema.set("toJSON", {
