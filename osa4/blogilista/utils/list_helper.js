abab6ad8b68e1ab6ddf918e63efc6febfcea4060
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-08-07 12:45:46 +0300

    tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     // ...
 };
 
+const totalLikes = (array) => {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i].likes;
+    }
+    return total;
+    return array.length === 0
+        ? 0
+        : array.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
 module.exports = {
     dummy,
+    totalLikes,
 };
