5fcfd608064c7342a6a223bb7107364e9f8be016
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-08-07 13:58:04 +0300

    tehtävä 4.5*

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -3,17 +3,22 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (array) => {
-    let total = 0;
-    for (let i = 0; i < array.length; i++) {
-        total += array[i].likes;
-    }
-    return total;
     return array.length === 0
         ? 0
         : array.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favouriteBlog = (array) => {
+    return array.length === 0
+        ? 0
+        : array.reduce(
+              (max, blog) => (blog.likes > max.likes ? blog : max),
+              array[0],
+          );
+};
+
 module.exports = {
     dummy,
     totalLikes,
+    favouriteBlog,
 };
