b4ff102839f83905b2f756bbf58819b0dc07d9d7
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-08-31 13:04:14 +0300

    tehtävä 4.11*

--- osa4/blogilista/tests/blogi_api.test.js
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -84,6 +84,25 @@ test("a valid blog can be added ", async () => {
     assert(contents.includes("Testiblogi"));
 });
 
+test("blog without likes defaults to 0", async () => {
+    const newBlog = {
+        title: "Testiblogi",
+        author: "Testaaja",
+        url: "https://testi.com",
+    };
+
+    await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+
+    const addedBlog = response.body.find((blog) => blog.title === "Testiblogi");
+    assert.strictEqual(addedBlog.likes, 0);
+});
+
 after(async () => {
     await mongoose.connection.close();
 });
