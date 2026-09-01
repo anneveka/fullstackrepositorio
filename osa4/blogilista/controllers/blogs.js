eb646f412b443500fdd2531282fc2abcce7925ac
anneveka <anna.e.karkkainen@student.jyu.fi>
2026-09-01 10:56:36 +0300

    tehtävä 4.13

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,22 +2,18 @@ const blogsRouter = require("express").Router();
 const { request } = require("express");
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
+blogsRouter.get("/", async (request, response) => {
+    const blogs = await Blog.find({});
+    response.json(blogs);
 });
 
-blogsRouter.get("/:id", (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then((blog) => {
-            if (blog) {
-                response.json(blog);
-            } else {
-                response.status(404).end();
-            }
-        })
-        .catch((error) => next(error));
+blogsRouter.get("/:id", async (request, response) => {
+    const blog = await Blog.findById(request.params.id);
+    if (blog) {
+        response.json(blog);
+    } else {
+        response.status(404).end();
+    }
 });
 
 blogsRouter.post("/", async (request, response) => {
@@ -32,6 +28,11 @@ blogsRouter.post("/", async (request, response) => {
 
     const savedBlog = await blog.save();
     response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+    await Blog.findByIdAndDelete(request.params.id);
+    response.status(204).end();
 });
 
 blogsRouter.put("/:id", (request, response, next) => {
--- osa4/blogilista/tests/blogi_api.test.js
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -101,6 +101,19 @@ test("blog without title is not added", async () => {
     assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
 });
 
+test("blog can be deleted", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    const contents = blogsAtEnd.map((b) => b.title);
+    assert(!contents.includes(blogToDelete.title));
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+});
+
 after(async () => {
     await mongoose.connection.close();
 });
